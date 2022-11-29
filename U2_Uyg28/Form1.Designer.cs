971796494b249a52f4eb3b06839ba6a1dcec6e9f
BIL-LAB5PC01\Ogrenci <123@123.COM>
2022-11-29 13:48:35 +0300

    no message

--- U2_Uyg28/Form1.Designer.cs
+++ U2_Uyg28/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace U2_SS_66_2
+﻿namespace U2_Uyg28
 {
     partial class Form1
     {
@@ -35,16 +35,16 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(80, 38);
+            this.listBox1.Location = new System.Drawing.Point(73, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 121);
+            this.listBox1.Size = new System.Drawing.Size(120, 147);
             this.listBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(101, 165);
+            this.button1.Location = new System.Drawing.Point(103, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 78);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
