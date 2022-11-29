6025e2ef70dd0993e23c801bb386e89d158fcce6
BIL-LAB5PC01\Ogrenci <123@123.COM>
2022-11-29 16:00:21 +0300

    no message

---
+++ U2_GoldSoru2/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace U2_GoldSoru2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Random random = new Random();
+                pictureBox1.Left = pictureBox1.Left + random.Next(1, 20);
+                pictureBox2.Left = pictureBox2.Left + random.Next(1, 20);
+            }
+        }
+    }
+}
