75931e37d5d547456d2bea587eb4197b2458cd12
BIL-LAB5PC06\Ogrenci <123@123.COM>
2022-12-20 12:19:04 +0300

    no message

---
+++ U2_GoldSoru7/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace U2_GoldSoru7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.FormattingEnabled = true;
+            this.ListBox1.Location = new System.Drawing.Point(2, 3);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(120, 238);
+            this.ListBox1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "En Küçük Sayı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "En Büyük Sayı";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 37);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Sayıları  Yeniden Dağıt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 45);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "En Küçük Ve En Büyük Sayılı BUL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 309);
+            this.Controls.Add(this.ListBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ U2_GoldSoru7/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_GoldSoru7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int rsayi = 0, x;
+            for (x = 0; x < 100; x++)
+            {
+
+                rsayi = r.Next(1, 100);
+                ListBox1.Items.Add(rsayi);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int enBuyuk = Convert.ToInt32(ListBox1.Items[0]), sayi;
+            for (int i = 0; i < ListBox1.Items.Count; i++)
+            {
+                sayi = Convert.ToInt32(ListBox1.Items[i]);
+                if (sayi > enBuyuk)
+                {
+                    enBuyuk = sayi;
+                }
+            }
+            label3.Text = "Listedeki En Büyük Sayı =" + enBuyuk.ToString();
+
+            int enkucuk = Convert.ToInt32(ListBox1.Items[0]), sayı;
+            for (int i = 0; i < ListBox1.Items.Count; i++)
+            {
+                sayı = Convert.ToInt32(ListBox1.Items[i]);
+                if (sayı < enkucuk) 
+                {
+                    enkucuk = sayı;
+                }
+                label4.Text = "Listedeki En Küçük Sayı=" + enkucuk.ToString();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            Random r = new Random();
+            int rsayı = 0; 
+
+            }
+    }
+}
+
+
+
+
+
+
+
+
+
+
