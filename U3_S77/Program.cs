eda100d333b95c23588def67af5e3b4362b1c738
BIL-LAB5PC06\Ogrenci <123@123.COM>
2022-12-27 16:14:54 +0300

    no message

---
+++ U3_S77/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(9, 11);
+
+            Console.WriteLine("Dikdörtgenin alanı:" + dikdortgen.AlanHesapla());
+
+            Console.WriteLine("Dikdörtgenin Çevrei:" + dikdortgen.CevreHesapla());
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+
+    class Dikdortgen
+
+    {
+        private int a, b;
+
+        public Dikdortgen(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int AlanHesapla()
+        {
+            return a * b;
+
+        }
+        public int CevreHesapla()
+        {
+
+            return 2 * (a + b);
+        }
+
+    }
+}
---
+++ U3_SS78/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire daire = new Daire();
+
+            Console.WriteLine("dairenin alanı:" + daire.Alan(4));
+            Console.WriteLine("dairenin çevresi:" + daire.Cevre(6));
+
+            Console.ReadLine();
+        }
+    }
+
+    class Daire
+    {
+        public double Alan(double r)
+        {
+            return 3.14 * r * r;
+        }
+
+        public double Cevre(double r)
+        {
+            return (2 * Math.PI * r);
+
+        }
+    }
+}
