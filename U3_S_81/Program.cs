f61345ec49cc8730efc8b11cb771205a35405fc9
BIL-LAB1PC11\Ogrenci <123@123.COM>
2023-01-03 16:37:54 +0300

    no message

---
+++ U3_S_81/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ucgen
+    {
+        int a;
+        int b;
+        int c;
+        public int A
+        {
+            get { return a; }
+
+            set
+            {
+                if (value<)
+                {
+
+                }
+            }
+        }
+        
+        
+          
+    
+
+
+
+}
---
+++ U3_S_91/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_91
+{
+    class kisi
+    {
+        int yas;
+        string ad;
+        public kisi()
+        {
+            yas = 19;
+            ad = "Mehmet olgun";
+            Console.WriteLine("Yapıcı metot çalıştı.");
+        }
+        public int yas
+        { 
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
