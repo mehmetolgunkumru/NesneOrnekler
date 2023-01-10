57f9fd4225f2de029d0ae20d32c63bfcf8a6aadc
BIL-LAB5PC06\Ogrenci <123@123.COM>
2023-01-10 13:58:43 +0300

    no message

---
+++ U3_SS_86/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS_86
+{
+    public static bool TekCift(int sayi)
+    {
+        if (sayi % 2 == 1)
+        {
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (sayi % 2 == 1)
+            {
+
+                //Sayı çift ise
+
+                return false;
+
+            }
+
+            return true;
+
+        }
+
+        function TekCift($sayi)
+
+        {
+
+            if (sayi % 2 == 1)
+            {
+
+                return true;
+
+            }
+
+            return false;
+
+        }
+
+        bool TekCift(int sayi)
+
+        {
+
+            if (sayi % 2 == 1)
+            {
+
+                return true;
+
+            }
+
+            return false;
+
+        }
+
+
+    }
+}
+}
---
+++ U3_S_100/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_100
+{
+    class OkulPersonel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    public class Ogretmen : OkulPersonel
+    { 
+
+}
+
