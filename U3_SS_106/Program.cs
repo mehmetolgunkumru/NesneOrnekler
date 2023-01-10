975dd3814ad3feb8d474ed4fe10e39e03f35a95b
BIL-LAB5PC06\Ogrenci <123@123.COM>
2023-01-10 15:57:12 +0300

    no message

---
+++ U3_SS_106/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS_106
+{
+    public abstract class Kutuphane
+    {
+        public abstract void Oku();
+    }
+    public class Kıtap : Kutuphane
+    {
+        public override void Oku()
+        {
+            Console.WriteLine("OKundu");
+        }
+
+        public class Dergı : Kutuphane
+        {
+            public override void Oku()
+            {
+                Console.WriteLine("OKundu");
+            }
+        }
+
+        public class ansıklopedı : Kutuphane
+        {
+            public override void Oku()
+            {
+                Console.WriteLine("OKundu");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kıtap oto = new Kıtap
+            {
+
+            };
+            oto.Oku();
+            {
+            }
+        }
+    }
+}
